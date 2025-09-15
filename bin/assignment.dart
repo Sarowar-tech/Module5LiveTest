132c22153ed1549546634912b6c727ffc0945ec2
Sarowar-tech <sarowar7996@gmail.com>
2025-09-15 21:06:17 +0600

    Update assignment.dart

--- bin/assignment.dart
+++ bin/assignment.dart
@@ -1,3 +1,29 @@
-void main(){
+main(){
+Book book = Book('Gitanjalee','Rabindranath Tagor',120);
+book.tittle = 'Gitanjalee';
+book.author = 'Robindranath Tagor';
+book.price = 120;
+book.writer();
+book.value();
 
+}
+
+
+class Book{
+  String ? tittle;
+  String ? author;
+  int ? price;
+
+  Book(this.tittle,this.author,this.price){
+    print('The name of the book is GitanJali');
+
+}
+
+
+writer(){
+    print('The Writer is $author');
+}
+value(){
+    print('The price of the Book is tk$price');
+}
 }
