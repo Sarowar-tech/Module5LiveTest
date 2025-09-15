30047a2ee056c13ca48ca26346f831b47f9e477d
Sarowar-tech <sarowar7996@gmail.com>
2025-09-15 21:59:24 +0600

    Update m5assignment.dart

--- bin/m5assignment.dart
+++ bin/m5assignment.dart
@@ -1,57 +1,39 @@
-import 'package:args/args.dart';
+main(){
 
-const String version = '0.0.1';
 
-ArgParser buildParser() {
-  return ArgParser()
-    ..addFlag(
-      'help',
-      abbr: 'h',
-      negatable: false,
-      help: 'Print this usage information.',
-    )
-    ..addFlag(
-      'verbose',
-      abbr: 'v',
-      negatable: false,
-      help: 'Show additional command output.',
-    )
-    ..addFlag('version', negatable: false, help: 'Print the tool version.');
+  Book book1 = Book();
+  book1.bookName = 'Gitanjalee';
+  book1.price = 150;
+  book1.author = 'Rabindranath Tagor';
+  Book book2 = Book();
+  book2.bookName = 'Kalbela';
+  book2.price = 140;
+  book2.author = 'Smaresh Majumder';
+  book1.bookTittle();
+  book1.writer();
+  book1.value();
+  book2.bookTittle();
+  book2.writer();
+  book2.value();
 }
 
-void printUsage(ArgParser argParser) {
-  print('Usage: dart m5assignment.dart <flags> [arguments]');
-  print(argParser.usage);
-}
 
-void main(List<String> arguments) {
-  final ArgParser argParser = buildParser();
-  try {
-    final ArgResults results = argParser.parse(arguments);
-    bool verbose = false;
+class Book{
+  late String bookName;
+  late int price;
+  late String author;
 
-    // Process the parsed arguments.
-    if (results.flag('help')) {
-      printUsage(argParser);
-      return;
-    }
-    if (results.flag('version')) {
-      print('m5assignment version: $version');
-      return;
-    }
-    if (results.flag('verbose')) {
-      verbose = true;
-    }
 
-    // Act on the arguments provided.
-    print('Positional arguments: ${results.rest}');
-    if (verbose) {
-      print('[VERBOSE] All arguments: ${results.arguments}');
-    }
-  } on FormatException catch (e) {
-    // Print usage information if an invalid argument was provided.
-    print(e.message);
-    print('');
-    printUsage(argParser);
+
+  bookTittle(){
+    print('Book name is $bookName');
+  }
+  writer(){
+    print('Author is $author');
+  }
+  value(){
+    print('Price is $price');
   }
 }
+ 
+   
